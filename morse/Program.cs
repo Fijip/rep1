658adf5ddba57f5ae4816f382dd1ed9f4e790ed6
Filip <fijip@wp.pl>
2018-12-10 18:12:46 +0100

    jeden

--- morse/Program.cs
+++ morse/Program.cs
@@ -4,12 +4,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace morse
+namespace ConsoleApp4
 {
     class Program
     {
-        static void Main(string[] args)
+        static Dictionary<char, string> kodyMorse;
+
+        static void Main(string[] args)//główna funkcja
         {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Biblioteka();
+            getUserInput();
+        }
+
+        private static void Biblioteka()
+        {
+            char dot = '.';
+            char dash = '−';
+
+            kodyMorse = new Dictionary<char, string>()
+            {
+                {'a', string.Concat(dot, dash)},
+                {'b', string.Concat(dash, dot, dot, dot)},
+                {'c', string.Concat(dash, dot, dash, dot)},
+                {'d', string.Concat(dash, dot, dot)},
+                {'e', dot.ToString()},
+                {'f', string.Concat(dot, dot, dash, dot)},
+                {'g', string.Concat(dash, dash, dot)},
+                {'h', string.Concat(dot, dot, dot, dot)},
+                {'i', string.Concat(dot, dot)},
+                {'j', string.Concat(dot, dash, dash, dash)},
+                {'k', string.Concat(dash, dot, dash)},
+                {'l', string.Concat(dot, dash, dot, dot)},
+                {'m', string.Concat(dash, dash)},
+                {'n', string.Concat(dash, dot)},
+                {'o', string.Concat(dash, dash, dash)},
+                {'p', string.Concat(dot, dash, dash, dot)},
+                {'q', string.Concat(dash, dash, dot, dash)},
+                {'r', string.Concat(dot, dash, dot)},
+                {'s', string.Concat(dot, dot, dot)},
+                {'t', string.Concat(dash)},
+                {'u', string.Concat(dot, dot, dash)},
+                {'v', string.Concat(dot, dot, dot, dash)},
+                {'w', string.Concat(dot, dash, dash)},
+                {'x', string.Concat(dash, dot, dot, dash)},
+                {'y', string.Concat(dash, dot, dash, dash)},
+                {'z', string.Concat(dash, dash, dot, dot)},
+                {'0', string.Concat(dash, dash, dash, dash, dash)},
+                {'1', string.Concat(dot, dash, dash, dash, dash)},
+                {'2', string.Concat(dot, dot, dash, dash, dash)},
+                {'3', string.Concat(dot, dot, dot, dash, dash)},
+                {'4', string.Concat(dot, dot, dot, dot, dash)},
+                {'5', string.Concat(dot, dot, dot, dot, dot)},
+                {'6', string.Concat(dash, dot, dot, dot, dot)},
+                {'7', string.Concat(dash, dash, dot, dot, dot)},
+                {'8', string.Concat(dash, dash, dash, dot, dot)},
+                {'9', string.Concat(dash, dash, dash, dash, dot)}
+            };
+        }
+
+        public static void getUserInput()
+        {
+            string tekstinput;
+            Console.WriteLine("Podaj tekst");
+            tekstinput = Console.ReadLine();
+            tekstinput = tekstinput.ToLower();
+            Console.WriteLine("Twoj tekst w kodzie morsa: " + translate(tekstinput));
+            Console.ReadLine();
+        }
+
+        private static string translate(string input)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (char character in input)
+            {
+                if (kodyMorse.ContainsKey(character))
+                {
+                    sb.Append(kodyMorse[character] + " ");
+                }
+                else if (character == ' ')
+                {
+                    sb.Append("/ ");
+                }
+                else
+                {
+                    sb.Append(character + " ");
+                }
+            }
+            return sb.ToString();
         }
     }
 }
